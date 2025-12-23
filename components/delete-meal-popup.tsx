f0a9c0da74ab17c523016a69dcf46440edc5eb14
LamInThisWeirdWorld <lamishere1715@gmail.com>
2025-12-23 20:39:57 +1100

    improve ui for delete action

--- components/delete-meal-popup.tsx
+++ components/delete-meal-popup.tsx
@@ -13,22 +13,36 @@ import {
 } from '@/components/ui/alert-dialog';
 import DeleteButton from './deleteButton';
 import deleteMeal from '@/app/action/deleteMeal';
+import { useState } from 'react';
 
 export function DeleteMealPopup({ mealId }: { mealId: string }) {
   const router = useRouter();
+  const [onDeleting, setOnDeleting] = useState(false);
 
   const handleDelete = async () => {
-    await deleteMeal(mealId);
-    router.push('/');
+    if (onDeleting) return;
+
+    try {
+      setOnDeleting(true);
+      await deleteMeal(mealId);
+      router.push('/');
+    } catch (err) {
+      console.log('error deleting meal');
+      setOnDeleting(false);
+    }
   };
   return (
     <AlertDialog>
       <AlertDialogTrigger>
         <button
           //   onClick={handleDelete}
-          className="right-180 flex h-10 w-30 cursor-pointer items-center justify-center self-end rounded-2xl border-2 border-[#F04D23] text-[20px] text-[#F04D23] transition hover:bg-[#F04D23] hover:text-white"
+          className={`right-180 flex h-10 w-30 items-center justify-center self-end rounded-2xl border-2 transition ${
+            onDeleting
+              ? 'cursor-not-allowed border-gray-400 text-gray-400 opacity-50'
+              : 'cursor-pointer border-[#F04D23] text-[20px] text-[#F04D23] hover:bg-[#F04D23] hover:text-white'
+          } `}
         >
-          Delete
+          {onDeleting ? 'Deleting...' : 'Delete'}
         </button>
       </AlertDialogTrigger>
       <AlertDialogContent>
